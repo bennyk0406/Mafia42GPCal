2bab3864d42553d526b77452bef55a8ee6dd09d0
bennyk0406 <kbenny0406@gmail.com>
2021-09-22 23:05:22 +0900

    v0.2.1

---
+++ itemData.js
@@ -0,0 +1,481 @@
+const itemData = {
+    "high": [
+        {
+            "name": "우주 달토끼 짐승인간",
+            "probability": 2.34,
+            "isEquipItem": true
+        },
+        {
+            "name": "우주 감시자 용병",
+            "probability": 2.34,
+            "isEquipItem": true
+        },
+        {
+            "name": "용맹의 증표 테두리",
+            "probability": 2.34,
+            "isEquipItem": true
+        },
+        {
+            "name": "우주복 헬멧 테두리",
+            "probability": 2.34,
+            "isEquipItem": true
+        },
+        {
+            "name": "호랑이의 꼬리",
+            "probability": 2.34,
+            "isEquipItem": true
+        },
+        {
+            "name": "달토끼의 우주패드",
+            "probability": 2.34,
+            "isEquipItem": true
+        },
+        {
+            "name": "달토끼의 우주 지갑",
+            "probability": 1.56,
+            "isEquipItem": true
+        },
+        {
+            "name": "홍단 스파이",
+            "probability": 1.17,
+            "isEquipItem": true
+        },
+        {
+            "name": "청단 영매",
+            "probability": 1.17,
+            "isEquipItem": true
+        },
+        {
+            "name": "보름달이 들어준 소원",
+            "probability": 1.17,
+            "isEquipItem": true
+        },
+        {
+            "name": "한가위 기념 테두리",
+            "probability": 1.17,
+            "isEquipItem": true
+        },
+        {
+            "name": "화투 42",
+            "probability": 1.17,
+            "isEquipItem": true
+        },
+        {
+            "name": "신비한 합창",
+            "probability": 0.585,
+            "isEquipItem": true
+        },
+        {
+            "name": "토끼 지갑",
+            "probability": 1.17,
+            "isEquipItem": true
+        },
+        {
+            "name": "돼지 지갑",
+            "probability": 1.17,
+            "isEquipItem": true
+        },
+        {
+            "name": "만월의 짐승인간",
+            "probability": 1.17,
+            "isEquipItem": true
+        },
+        {
+            "name": "달토끼 영매",
+            "probability": 1.17,
+            "isEquipItem": true
+        },
+        {
+            "name": "방아꾼 건달",
+            "probability": 1.17,
+            "isEquipItem": true
+        },
+        {
+            "name": "토끼 테두리",
+            "probability": 1.17,
+            "isEquipItem": true
+        },
+        {
+            "name": "청룡 : 정의의 파도 경찰",
+            "probability": 1.17,
+            "isEquipItem": true
+        },
+        {
+            "name": "백호 : 치유의 바람 의사",
+            "probability": 1.17,
+            "isEquipItem": true
+        },
+        {
+            "name": "구미호 마담",
+            "probability": 1.17,
+            "isEquipItem": true
+        },
+        {
+            "name": "도깨비 기자",
+            "probability": 1.17,
+            "isEquipItem": true
+        },
+        {
+            "name": "귀면문 테두리",
+            "probability": 1.17,
+            "isEquipItem": true
+        },
+        {
+            "name": "여우령 테두리",
+            "probability": 1.17,
+            "isEquipItem": true
+        },
+        {
+            "name": "아가씨의 아얌",
+            "probability": 1.17,
+            "isEquipItem": true
+        },
+        {
+            "name": "도깨비의 허리장식",
+            "probability": 1.17,
+            "isEquipItem": true
+        },
+        {
+            "name": "자색 비단 지갑",
+            "probability": 0.585,
+            "isEquipItem": true
+        },
+        {
+            "name": "청룡의 뿔",
+            "probability": 4.27,
+            "isEquipItem": false
+        },
+        {
+            "name": "백호의 이빨",
+            "probability": 4.27,
+            "isEquipItem": false
+        },
+        {
+            "name": "한가위 기념 엽서",
+            "probability": 9.15,
+            "isEquipItem": false
+        },
+        {
+            "name": "보름달 엽서",
+            "probability": 9.15,
+            "isEquipItem": false
+        },
+        {
+            "name": "한가위 기념 확성기",
+            "probability": 9.15,
+            "isEquipItem": false
+        },
+        {
+            "name": "호랑이 꼬리",
+            "probability": 4.27,
+            "isEquipItem": false
+        },
+        {
+            "name": "토끼 꼬리",
+            "probability": 4.27,
+            "isEquipItem": false
+        },
+        {
+            "name": "낡은 징벌의 엽서",
+            "probability": 3.05,
+            "isEquipItem": false
+        },
+        {
+            "name": "징벌의 엽서",
+            "probability": 3.05,
+            "isEquipItem": false
+        },
+        {
+            "name": "깜짝 엽서",
+            "probability": 3.05,
+            "isEquipItem": false
+        },
+        {
+            "name": "주사위 엽서",
+            "probability": 3.05,
+            "isEquipItem": false
+        },
+        {
+            "name": "4티어 카드",
+            "probability": 3.05,
+            "isEquipItem": false
+        },
+        {
+            "name": "루블",
+            "probability": 0.61,
+            "isEquipItem": false
+        },
+        {
+            "name": "루나",
+            "probability": 0.61,
+            "isEquipItem": false
+        }
+    ],
+    "low": [
+        {
+            "name": "우주 달토끼 짐승인간",
+            "probability": 0.48,
+            "isEquipItem": true
+        },
+        {
+            "name": "우주 감시자 용병",
+            "probability": 0.48,
+            "isEquipItem": true
+        },
+        {
+            "name": "용맹의 증표 테두리",
+            "probability": 0.48,
+            "isEquipItem": true
+        },
+        {
+            "name": "우주복 헬멧 테두리",
+            "probability": 0.48,
+            "isEquipItem": true
+        },
+        {
+            "name": "호랑이의 꼬리",
+            "probability": 0.48,
+            "isEquipItem": true
+        },
+        {
+            "name": "달토끼의 우주패드",
+            "probability": 0.48,
+            "isEquipItem": true
+        },
+        {
+            "name": "달토끼의 우주 지갑",
+            "probability": 0.32,
+            "isEquipItem": true
+        },
+        {
+            "name": "홍단 스파이",
+            "probability": 0.24,
+            "isEquipItem": true
+        },
+        {
+            "name": "청단 영매",
+            "probability": 0.24,
+            "isEquipItem": true
+        },
+        {
+            "name": "보름달이 들어준 소원",
+            "probability": 0.24,
+            "isEquipItem": true
+        },
+        {
+            "name": "한가위 기념 테두리",
+            "probability": 0.24,
+            "isEquipItem": true
+        },
+        {
+            "name": "화투 42",
+            "probability": 0.24,
+            "isEquipItem": true
+        },
+        {
+            "name": "신비한 합창",
+            "probability": 0.12,
+            "isEquipItem": true
+        },
+        {
+            "name": "토끼 지갑",
+            "probability": 0.24,
+            "isEquipItem": true
+        },
+        {
+            "name": "돼지 지갑",
+            "probability": 0.24,
+            "isEquipItem": true
+        },
+        {
+            "name": "만월의 짐승인간",
+            "probability": 0.24,
+            "isEquipItem": true
+        },
+        {
+            "name": "달토끼 영매",
+            "probability": 0.24,
+            "isEquipItem": true
+        },
+        {
+            "name": "방아꾼 건달",
+            "probability": 0.24,
+            "isEquipItem": true
+        },
+        {
+            "name": "토끼 테두리",
+            "probability": 0.24,
+            "isEquipItem": true
+        },
+        {
+            "name": "청룡 : 정의의 파도 경찰",
+            "probability": 0.24,
+            "isEquipItem": true
+        },
+        {
+            "name": "백호 : 치유의 바람 의사",
+            "probability": 0.24,
+            "isEquipItem": true
+        },
+        {
+            "name": "구미호 마담",
+            "probability": 0.24,
+            "isEquipItem": true
+        },
+        {
+            "name": "도깨비 기자",
+            "probability": 0.24,
+            "isEquipItem": true
+        },
+        {
+            "name": "귀면문 테두리",
+            "probability": 0.24,
+            "isEquipItem": true
+        },
+        {
+            "name": "여우령 테두리",
+            "probability": 0.24,
+            "isEquipItem": true
+        },
+        {
+            "name": "아가씨의 아얌",
+            "probability": 0.24,
+            "isEquipItem": true
+        },
+        {
+            "name": "도깨비의 허리장식",
+            "probability": 0.24,
+            "isEquipItem": true
+        },
+        {
+            "name": "자색 비단 지갑",
+            "probability": 0.12,
+            "isEquipItem": true
+        },
+        {
+            "name": "명절선물 테러리스트",
+            "probability": 10.12,
+            "isEquipItem": true
+        },
+        {
+            "name": "풍년농부 도굴꾼",
+            "probability": 10.12,
+            "isEquipItem": true
+        },
+        {
+            "name": "청룡의 뿔",
+            "probability": 3.68,
+            "isEquipItem": false
+        },
+        {
+            "name": "백호의 이빨",
+            "probability": 3.68,
+            "isEquipItem": false
+        },
+        {
+            "name": "한가위 기념 확성기",
+            "probability": 10.12,
+            "isEquipItem": false
+        },
+        {
+            "name": "보름달 엽서",
+            "probability": 10.12,
+            "isEquipItem": false
+        },
+        {
+            "name": "한가위 기념 엽서",
+            "probability": 10.12,
+            "isEquipItem": false
+        },
+        {
+            "name": "호랑이 꼬리",
+            "probability": 3.68,
+            "isEquipItem": false
+        },
+        {
+            "name": "토끼 꼬리",
+            "probability": 3.68,
+            "isEquipItem": false
+        },
+        {
+            "name": "징벌의 엽서",
+            "probability": 1.84,
+            "isEquipItem": false
+        },
+        {
+            "name": "깜짝 엽서",
+            "probability": 1.84,
+            "isEquipItem": false
+        },
+        {
+            "name": "큐피드의 화살",
+            "probability": 1.84,
+            "isEquipItem": false
+        },
+        {
+            "name": "마법의 염색약",
+            "probability": 1.84,
+            "isEquipItem": false
+        },
+        {
+            "name": "스포이트",
+            "probability": 1.84,
+            "isEquipItem": false
+        },
+        {
+            "name": "신분증",
+            "probability": 1.84,
+            "isEquipItem": false
+        },
+        {
+            "name": "부고기사",
+            "probability": 1.84,
+            "isEquipItem": false
+        },
+        {
+            "name": "고급 엽서",
+            "probability": 1.84,
+            "isEquipItem": false
+        },
+        {
+            "name": "사망 광고판",
+            "probability": 1.84,
+            "isEquipItem": false
+        },
+        {
+            "name": "확성기",
+            "probability": 1.84,
+            "isEquipItem": false
+        },
+        {
+            "name": "고대의 제작서",
+            "probability": 1.84,
+            "isEquipItem": false
+        },
+        {
+            "name": "마법의 연마제",
+            "probability": 1.84,
+            "isEquipItem": false
+        },
+        {
+            "name": "명장의 망치",
+            "probability": 1.84,
+            "isEquipItem": false
+        },
+        {
+            "name": "3티어 카드",
+            "probability": 1.84,
+            "isEquipItem": false
+        },
+        {
+            "name": "루블",
+            "probability": 0.46,
+            "isEquipItem": false
+        },
+        {
+            "name": "루나",
+            "probability": 0.46,
+            "isEquipItem": false
+        }
+    ]
+};
---
+++ script.js
@@ -0,0 +1,94 @@
+const imageUrlList = [
+    'https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2Fb7ca3D%2FbtrfBro6Kr7%2FfBhxyIDBz9K0ucOMYLjS6K%2Fimg.png',
+    'https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FrcuWD%2FbtrfIZE6mfR%2FTWacfWKMkhd0pnLZVykyJK%2Fimg.png'
+];
+
+const onClickCheckbox = function(level) {
+    const checkList = [...document.querySelectorAll('input[type="checkbox"]')].map(e => e.checked);
+    const itemList = itemData[level].map(value => ({...value}));
+    let checkedProbability = 0;
+    for (let i = 0; i < checkList.length; i++) {
+        if (checkList[i]) {
+            checkedProbability += itemList[i].probability; 
+            itemList[i].probability = 0; 
+        }
+    }
+    for (let j = 0; j < itemList.length; j++) {
+        itemList[j].probability = Math.round(itemList[j].probability * 100 / (100 - checkedProbability) * 1000) / 1000; 
+    }
+    const probabilityList = [...document.querySelectorAll('.probability')]; 
+    for (let k = 0; k < probabilityList.length; k++) {
+        probabilityList[k].innerText = `${itemList[k].probability}%`;
+    }
+    const equipItemList = itemList.filter(i => i.isEquipItem); 
+    const equipItemProbability = Math.round(equipItemList.map(i => i.probability).reduce((a,b) => a + b) * 1000) / 1000; 
+    const totalProbability = document.getElementById('total-probability');
+    totalProbability.innerText = `장착 아이템 확률 : ${equipItemProbability}%`;
+}
+
+const onClickChangeButton = function() {
+    const img = document.getElementById('change-gashapon-img'); 
+    if (img.src === imageUrlList[0]) {
+        img.setAttribute('src', imageUrlList[1]);
+        setTable(itemData.high, 'high');
+    } else {
+        img.setAttribute('src', imageUrlList[0]);
+        setTable(itemData.low, 'low');
+    }
+};
+
+const setTable = function(itemList, level) {
+    const table = document.getElementById('item-table');
+    table.innerHTML = `<tr>
+        <th>
+            보유 여부
+        </th>
+        <th>
+            아이템
+        </th>
+        <th>
+            확률
+        </th>
+    </tr>`;
+    const equipItemList = itemList.filter(i => i.isEquipItem);
+    const otherItemList = itemList.filter(i => !i.isEquipItem);
+    for (const i of equipItemList) {
+        const item = document.createElement('tr');
+        item.innerHTML = `<td class='has-item'>
+            <input type='checkbox' onclick='onClickCheckbox("${level}")'>
+        </td>
+        <td class='name'>
+            ${i.name}
+        </td>
+        <td class='probability'>
+            ${i.probability}%
+        </td>`;
+        table.append(item);
+    }
+    for (const j of otherItemList) {
+        const item = document.createElement('tr');
+        item.innerHTML = `<td>
+        </td>
+        <td class='name'>
+            ${j.name}
+        </td>
+        <td class='probability'>
+            ${j.probability}%
+        </td>`;
+        table.append(item);
+    }
+    const equipItemProbability = Math.round(equipItemList.map(i => i.probability).reduce((a,b) => a + b) * 1000) / 1000; 
+    const image = document.getElementById('change-gashapon-img');
+    switch (level) {
+        case 'high': 
+            image.outerHTML = `<img id='change-gashapon-img' src='${imageUrlList[1]}' alt='단계 변경 이미지' />`;
+            break;
+        case 'low':
+            image.outerHTML = `<img id='change-gashapon-img' src='${imageUrlList[0]}' alt='단계 변경 이미지' />`;
+            break;
+    }
+    const totalProbability = document.getElementById('total-probability');
+    totalProbability.innerText = `장착 아이템 확률 : ${equipItemProbability}%`;
+}
+
+setTable(itemData.high, 'high');
