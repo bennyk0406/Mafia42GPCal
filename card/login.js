8815d8d62c7e38fc514f14df9e97766f28795f73
bennyk0406 <kbenny0406@gmail.com>
2021-10-30 13:18:00 +0900

    v1.0.0

--- card/login.js
+++ card/login.js
@@ -1,4 +1,4 @@
-import { getAuth, getRedirectResult, GoogleAuthProvider } from "firebase/auth";
+import { getAuth, getRedirectResult, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-auth.js";
 
 const googleLogin = function () {
     const auth = getAuth();
