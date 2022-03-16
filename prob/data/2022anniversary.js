583971b9e55d5654d2ec98092d64b20c83068ef0
bennyk0406 <kbenny0406@gmail.com>
2022-03-17 01:57:58 +0900

    2022anniversary

---
+++ prob/data/2022anniversary.js
@@ -0,0 +1,481 @@
+const itemData = {
+    high: [
+        {
+            name: "가정교사 공무원",
+            probability: 2.5,
+            isEquipItem: true
+        },
+        {
+            name: "레이디 마담",
+            probability: 2.5,
+            isEquipItem: true
+        },
+        {
+            name: "녹슨 장미 테두리",
+            probability: 2.5,
+            isEquipItem: true
+        },
+        {
+            name: "파티용 드레스 테두리",
+            probability: 2.5,
+            isEquipItem: true
+        },
+        {
+            name: "말린 장미 회랑",
+            probability: 2.5,
+            isEquipItem: true
+        },
+        {
+            name: "고귀한 예법",
+            probability: 2.5,
+            isEquipItem: true
+        },
+        {
+            name: "저택의 비밀 명부",
+            probability: 1.6,
+            isEquipItem: true
+        },
+        {
+            name: "쇼퍼홀릭 영매",
+            probability: 1.2,
+            isEquipItem: true
+        },
+        {
+            name: "영앤리치 도둑",
+            probability: 1.2,
+            isEquipItem: true
+        },
+        {
+            name: "지름신 테두리",
+            probability: 1.2,
+            isEquipItem: true
+        },
+        {
+            name: "호화로운 액자 테두리",
+            probability: 1.2,
+            isEquipItem: true
+        },
+        {
+            name: "부의 대물림",
+            probability: 1.2,
+            isEquipItem: true
+        },
+        {
+            name: "아타셰 케이스",
+            probability: 1.2,
+            isEquipItem: true
+        },
+        {
+            name: "체크무늬 패턴 지갑",
+            probability: 1,
+            isEquipItem: true
+        },
+        {
+            name: "셰퍼드 경찰",
+            probability: 1.2,
+            isEquipItem: true
+        },
+        {
+            name: "허스키 도둑",
+            probability: 1.2,
+            isEquipItem: true
+        },
+        {
+            name: "3주년 마피아",
+            probability: 1.2,
+            isEquipItem: true
+        },
+        {
+            name: "추억의 영화감독 기자",
+            probability: 1.2,
+            isEquipItem: true
+        },
+        {
+            name: "추억의 영화배우 마담",
+            probability: 1.2,
+            isEquipItem: true,
+        },
+        {
+            name: "흥신소 상담실장 마녀",
+            probability: 1.2,
+            isEquipItem: true
+        },
+        {
+            name: "흥신소장 사립탐정",
+            probability: 1.2,
+            isEquipItem: true
+        },
+        {
+            name: "필름 테두리",
+            probability: 1.2,
+            isEquipItem: true
+        },
+        {
+            name: "접근 금지선 테두리",
+            probability: 1.2,
+            isEquipItem: true
+        },
+        {
+            name: "일급기밀 개굴 수첩",
+            probability: 1,
+            isEquipItem: true
+        },
+        {
+            name: "3주년 기념 명패",
+            probability: 1.2,
+            isEquipItem: true
+        },
+        {
+            name: "명예 흥신소 단원 명패",
+            probability: 1.2,
+            isEquipItem: true
+        },
+        {
+            name: "메이드 스파이",
+            probability: 1.2,
+            isEquipItem: true
+        },
+        {
+            name: "호스트 도굴꾼",
+            probability: 1.2,
+            isEquipItem: true
+        },
+        {
+            name: "메이드의 해골",
+            probability: 1.2,
+            isEquipItem: true
+        },
+        {
+            name: "저택에서의 추억",
+            probability: 1.2,
+            isEquipItem: true
+        },
+        {
+            name: "신비한 합창",
+            probability: 1,
+            isEquipItem: true
+        },
+        {
+            name: "루블",
+            probability: 1,
+            isEquipItem: false
+        },
+        {
+            name: "루나",
+            probability: 1,
+            isEquipItem: false
+        },
+        {
+            name: "마법의 염색약",
+            probability: 4,
+            isEquipItem: false
+        },
+        {
+            name: "깜짝 엽서",
+            probability: 4,
+            isEquipItem: false
+        },
+        {
+            name: "징벌의 엽서",
+            probability: 4,
+            isEquipItem: false
+        },
+        {
+            name: "낡은 징벌의 엽서",
+            probability: 4,
+            isEquipItem: false
+        },
+        {
+            name: "4티어 카드",
+            probability: 4,
+            isEquipItem: false
+        },
+        {
+            name: "3티어 카드",
+            probability: 4,
+            isEquipItem: false
+        },
+        {
+            name: "주사위 엽서",
+            probability: 4,
+            isEquipItem: false
+        },
+        {
+            name: "추천장",
+            probability: 6.3,
+            isEquipItem: false
+        },
+        {
+            name: "4주년 기념 엽서",
+            probability: 6.3,
+            isEquipItem: false
+        },
+        {
+            name: "달조각",
+            probability: 6.3,
+            isEquipItem: false
+        },
+        {
+            name: "강화 확성기",
+            probability: 6.3,
+            isEquipItem: false
+        }
+    ],
+    low: [
+        {
+            name: "가정교사 공무원",
+            probability: 0.65, 
+            isEquipItem: true
+        },
+        {
+            name: "레이디 마담",
+            probability: 0.65,
+            isEquipItem: true
+        },
+        {
+            name: "녹슨 장미 테두리",
+            probability: 0.65,
+            isEquipItem: true
+        },
+        {
+            name: "파티용 드레스 테두리",
+            probability: 0.65,
+            isEquipItem: true
+        },
+        {
+            name: "말린 장미 회랑",
+            probability: 0.65,
+            isEquipItem: true
+        },
+        {
+            name: "고귀한 예법",
+            probability: 0.65,
+            isEquipItem: true
+        },
+        {
+            name: "저택의 비밀 명부",
+            probability: 0.4,
+            isEquipItem: true
+        },
+        {
+            name: "쇼퍼홀릭 영매",
+            probability: 0.2,
+            isEquipItem: true
+        },
+        {
+            name: "영앤리치 도둑",
+            probability: 0.2,
+            isEquipItem: true
+        },
+        {
+            name: "지름신 테두리",
+            probability: 0.2,
+            isEquipItem: true
+        },
+        {
+            name: "호화로운 액자 테두리",
+            probability: 0.2,
+            isEquipItem: true
+        },
+        {
+            name: "부의 대물림",
+            probability: 0.2,
+            isEquipItem: true
+        },
+        {
+            name: "아타셰 케이스",
+            probability: 0.2,
+            isEquipItem: true
+        },
+        {
+            name: "체크무늬 패턴 지갑",
+            probability: 0.15,
+            isEquipItem: true
+        },
+        {
+            name: "셰퍼드 경찰",
+            probability: 0.2,
+            isEquipItem: true
+        },
+        {
+            name: "허스키 도둑",
+            probability: 0.2,
+            isEquipItem: true
+        },
+        {
+            name: "3주년 마피아",
+            probability: 0.2,
+            isEquipItem: true
+        },
+        {
+            name: "추억의 영화감독 기자",
+            probability: 0.2,
+            isEquipItem: true
+        },
+        {
+            name: "추억의 영화배우 마담",
+            probability: 0.2,
+            isEquipItem: true
+        },
+        {
+            name: "흥신소 상담실장 마녀",
+            probability: 0.2,
+            isEquipItem: true
+        },
+        {
+            name: "흥신소장 사립탐정",
+            probability: 0.2,
+            isEquipItem: true
+        },
+        {
+            name: "필름 테두리",
+            probability: 0.2,
+            isEquipItem: true
+        },
+        {
+            name: "접근 금지선 테두리",
+            probability: 0.2,
+            isEquipItem: true
+        },
+        {
+            name: "일급기밀 개굴 수첩",
+            probability: 0.15,
+            isEquipItem: true
+        },
+        {
+            name: "3주년 기념 명패",
+            probability: 0.2,
+            isEquipItem: true
+        },
+        {
+            name: "명예 흥신소 단원 명패",
+            probability: 0.2,
+            isEquipItem: true
+        },
+        {
+            name: "메이드 스파이",
+            probability: 0.2,
+            isEquipItem: true
+        },
+        {
+            name: "호스트 도굴꾼",
+            probability: 0.2,
+            isEquipItem: true
+        },
+        {
+            name: "메이드의 해골",
+            probability: 0.2,
+            isEquipItem: true
+        },
+        {
+            name: "저택에서의 추억",
+            probability: 0.2,
+            isEquipItem: true
+        },
+        {
+            name: "루블",
+            probability: 0.1,
+            isEquipItem: false
+        },
+        {
+            name: "루나",
+            probability: 0.1,
+            isEquipItem: false
+        },
+        {
+            name: "스포이트",
+            probability: 4.2,
+            isEquipItem: false
+        },
+        {
+            name: "마법의 염색약",
+            probability: 4.2,
+            isEquipItem: false
+        },
+        {
+            name: "신분증",
+            probability: 4.2,
+            isEquipItem: false
+        },
+        {
+            name: "사망 광고판",
+            probability: 4.2,
+            isEquipItem: false
+        },
+        {
+            name: "부고기사",
+            probability: 4.2,
+            isEquipItem: false
+        },
+        {
+            name: "고대의 제작서",
+            probability: 4.2,
+            isEquipItem: false
+        },
+        {
+            name: "마법의 연마제",
+            probability: 4.2,
+            isEquipItem: false
+        },
+        {
+            name: "명장의 망치",
+            probability: 4.2,
+            isEquipItem: false
+        },
+        {
+            name: "큐피드의 화살",
+            probability: 4.2,
+            isEquipItem: false
+        },
+        {
+            name: "고급 엽서",
+            probability: 4.2,
+            isEquipItem: false
+        },
+        {
+            name: "징벌의 엽서",
+            probability: 4.2,
+            isEquipItem: false
+        },
+        {
+            name: "깜짝 엽서",
+            probability: 4.2,
+            isEquipItem: false
+        },
+        {
+            name: "확성기",
+            probability: 4.2,
+            isEquipItem: false
+        },
+        {
+            name: "3티어 카드",
+            probability: 4.2,
+            isEquipItem: false
+        },
+        {
+            name: "2티어 카드",
+            probability: 4.2,
+            isEquipItem: false
+        },
+        {
+            name: "추천장",
+            probability: 7,
+            isEquipItem: false
+        },
+        {
+            name: "4주년 기념 엽서",
+            probability: 7,
+            isEquipItem: false
+        },
+        {
+            name: "달조각",
+            probability: 7,
+            isEquipItem: false
+        },
+        {
+            name: "강화 확성기",
+            probability: 7,
+            isEquipItem: false
+        }
+    ]
+};
