4faa726a1a93c7678585be5919573f156c78ee92
bennyk0406 <kbenny0406@gmail.com>
2022-05-04 17:29:09 +0900

    2022contest

---
+++ prob/data/2022contest.js
@@ -0,0 +1,511 @@
+const itemData = {
+    "high": [
+        {
+            "name": "산맥을 넘는 정치인",
+            "probability": 2,
+            "isEquipItem": true
+        },
+        {
+            "name": "키스하는 연인",
+            "probability": 2,
+            "isEquipItem": true
+        },
+        {
+            "name": "신고전주의 테두리",
+            "probability": 2,
+            "isEquipItem": true
+        },
+        {
+            "name": "아르 누보 테두리",
+            "probability": 2,
+            "isEquipItem": true
+        },
+        {
+            "name": "지도자의 망토",
+            "probability": 2,
+            "isEquipItem": true
+        },
+        {
+            "name": "평화로운 풍경화",
+            "probability": 2,
+            "isEquipItem": true
+        },
+        {
+            "name": "예술가의 수첩",
+            "probability": 1.5,
+            "isEquipItem": true
+        },
+        {
+            "name": "카네이션 테두리",
+            "probability": 1.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "장난감 이모티콘",
+            "probability": 1.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "벨보이 마피아",
+            "probability": 1.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "바텐더 스파이",
+            "probability": 1.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "트롤리 테두리",
+            "probability": 1.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "오래된 거울 테두리",
+            "probability": 1.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "주류 선반",
+            "probability": 1.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "룸 넘버",
+            "probability": 1.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "숙박인 명부",
+            "probability": 1,
+            "isEquipItem": true
+        },
+        {
+            "name": "화가 예언자",
+            "probability": 1.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "넓적 붓 명패",
+            "probability": 1.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "아크릴 붓 명패",
+            "probability": 1.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "빛의 팔레트 테두리",
+            "probability": 1.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "어둠의 팔레트 테두리",
+            "probability": 1.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "쇼퍼홀릭 영매",
+            "probability": 1.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "영앤리치 도둑",
+            "probability": 1.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "지름신 테두리",
+            "probability": 1.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "호화로운 액자 테두리",
+            "probability": 1.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "부의 대물림",
+            "probability": 1.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "아타셰 케이스",
+            "probability": 1.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "체크무늬 패턴 지갑",
+            "probability": 1,
+            "isEquipItem": true
+        },
+        {
+            "name": "야전병원의 의사",
+            "probability": 1.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "결의에 찬 시민",
+            "probability": 1.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "최전선의 자경단원",
+            "probability": 1.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "결사대원의 후드 테두리",
+            "probability": 1.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "홀스터 테두리",
+            "probability": 1.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "수제 볼트통",
+            "probability": 1.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "야전병원의 밤",
+            "probability": 1.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "신원미상의 일지",
+            "probability": 1,
+            "isEquipItem": true
+        },
+        {
+            "name": "신비한 합창",
+            "probability": 1,
+            "isEquipItem": true
+        },
+        {
+            "name": "마법의 염색약",
+            "probability": 4.1,
+            "isEquipItem": false
+        },
+        {
+            "name": "깜짝 엽서",
+            "probability": 4.1,
+            "isEquipItem": false
+        },
+        {
+            "name": "징벌의 엽서",
+            "probability": 4.1,
+            "isEquipItem": false
+        },
+        {
+            "name": "낡은 징벌의 엽서",
+            "probability": 4.1,
+            "isEquipItem": false
+        },
+        {
+            "name": "4티어 카드",
+            "probability": 4.1,
+            "isEquipItem": false
+        },
+        {
+            "name": "3티어 카드",
+            "probability": 4.1,
+            "isEquipItem": false
+        },
+        {
+            "name": "주사위 엽서",
+            "probability": 4.1,
+            "isEquipItem": false
+        },
+        {
+            "name": "장난감 확성기",
+            "probability": 7.7,
+            "isEquipItem": false
+        },
+        {
+            "name": "장난감 엽서",
+            "probability": 7.7,
+            "isEquipItem": false
+        },
+        {
+            "name": "불발탄",
+            "probability": 7.2,
+            "isEquipItem": false
+        }
+    ],
+    "low": [
+        {
+            "name": "산맥을 넘는 정치인",
+            "probability": 0.5,
+            "isEquipItem": true
+        },
+        {
+            "name": "키스하는 연인",
+            "probability": 0.5,
+            "isEquipItem": true
+        },
+        {
+            "name": "신고전주의 테두리",
+            "probability": 0.5,
+            "isEquipItem": true
+        },
+        {
+            "name": "아르 누보 테두리",
+            "probability": 0.5,
+            "isEquipItem": true
+        },
+        {
+            "name": "지도자의 망토",
+            "probability": 0.5,
+            "isEquipItem": true
+        },
+        {
+            "name": "평화로운 풍경화",
+            "probability": 0.5,
+            "isEquipItem": true
+        },
+        {
+            "name": "예술가의 수첩",
+            "probability": 0.35,
+            "isEquipItem": true
+        },
+        {
+            "name": "카네이션 테두리",
+            "probability": 0.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "장난감 이모티콘",
+            "probability": 0.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "벨보이 마피아",
+            "probability": 0.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "바텐더 스파이",
+            "probability": 0.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "트롤리 테두리",
+            "probability": 0.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "오래된 거울 테두리",
+            "probability": 0.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "주류 선반",
+            "probability": 0.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "룸 넘버",
+            "probability": 0.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "숙박인 명부",
+            "probability": 1,
+            "isEquipItem": true
+        },
+        {
+            "name": "화가 예언자",
+            "probability": 0.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "넓적 붓 명패",
+            "probability": 0.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "아크릴 붓 명패",
+            "probability": 0.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "빛의 팔레트 테두리",
+            "probability": 0.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "어둠의 팔레트 테두리",
+            "probability": 0.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "쇼퍼홀릭 영매",
+            "probability": 0.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "영앤리치 도둑",
+            "probability": 0.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "지름신 테두리",
+            "probability": 0.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "호화로운 액자 테두리",
+            "probability": 0.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "부의 대물림",
+            "probability": 0.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "아타셰 케이스",
+            "probability": 0.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "체크무늬 패턴 지갑",
+            "probability": 0.15,
+            "isEquipItem": true
+        },
+        {
+            "name": "야전병원의 의사",
+            "probability": 0.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "결의에 찬 시민",
+            "probability": 0.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "최전선의 자경단원",
+            "probability": 0.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "결사대원의 후드 테두리",
+            "probability": 0.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "홀스터 테두리",
+            "probability": 0.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "수제 볼트통",
+            "probability": 0.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "야전병원의 밤",
+            "probability": 0.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "신원미상의 일지",
+            "probability": 0.15,
+            "isEquipItem": true
+        },
+        {
+            "name": "스포이트",
+            "probability": 4.6,
+            "isEquipItem": false
+        },
+        {
+            "name": "마법의 염색약",
+            "probability": 4.6,
+            "isEquipItem": false
+        },
+        {
+            "name": "신분증",
+            "probability": 4.6,
+            "isEquipItem": false
+        },
+        {
+            "name": "사망 광고판",
+            "probability": 4.6,
+            "isEquipItem": false
+        },
+        {
+            "name": "부고기사",
+            "probability": 4.6,
+            "isEquipItem": false
+        },
+        {
+            "name": "고대의 제작서",
+            "probability": 4.6,
+            "isEquipItem": false
+        },
+        {
+            "name": "마법의 연마제",
+            "probability": 4.6,
+            "isEquipItem": false
+        },
+        {
+            "name": "명장의 망치",
+            "probability": 4.6,
+            "isEquipItem": false
+        },
+        {
+            "name": "큐피드의 화살",
+            "probability": 4.6,
+            "isEquipItem": false
+        },
+        {
+            "name": "고급 엽서",
+            "probability": 4.6,
+            "isEquipItem": false
+        },
+        {
+            "name": "징벌의 엽서",
+            "probability": 4.6,
+            "isEquipItem": false
+        },
+        {
+            "name": "깜짝 엽서",
+            "probability": 4.6,
+            "isEquipItem": false
+        },
+        {
+            "name": "확성기",
+            "probability": 4.6,
+            "isEquipItem": false
+        },
+        {
+            "name": "3티어 카드",
+            "probability": 4.6,
+            "isEquipItem": false
+        },
+        {
+            "name": "2티어 카드",
+            "probability": 4.6,
+            "isEquipItem": false
+        },
+        {
+            "name": "장난감 확성기",
+            "probability": 7.5,
+            "isEquipItem": false
+        },
+        {
+            "name": "장난감 엽서",
+            "probability": 7.5,
+            "isEquipItem": false
+        },
+        {
+            "name": "불발탄",
+            "probability": 7,
+            "isEquipItem": false
+        }
+    ]
+};
