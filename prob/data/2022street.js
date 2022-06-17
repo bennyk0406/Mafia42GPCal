bacc1f5b77e9b867fda96b815a2a136e0dec3813
bennyk0406 <kbenny0406@gmail.com>
2022-06-17 16:39:20 +0900

    2022street

---
+++ prob/data/2022street.js
@@ -0,0 +1,451 @@
+const itemData = {
+    high: [
+        {
+            "name": "황야의 승부사 건달",
+            "probability": 3,
+            "isEquipItem": true
+        },
+        {
+            "name": "판의 지배자 마술사",
+            "probability": 3,
+            "isEquipItem": true
+        },
+        {
+            "name": "지배자의 코트 테두리",
+            "probability": 3,
+            "isEquipItem": true
+        },
+        {
+            "name": "서부의 모험 테두리",
+            "probability": 3,
+            "isEquipItem": true
+        },
+        {
+            "name": "승자의 속임수",
+            "probability": 3,
+            "isEquipItem": true
+        },
+        {
+            "name": "마지막 베팅",
+            "probability": 3,
+            "isEquipItem": true
+        },
+        {
+            "name": "승부사의 지갑",
+            "probability": 2,
+            "isEquipItem": true
+        },
+        {
+            "name": "클로버의 퀸 심리학자",
+            "probability": 1.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "다이아몬드의 잭 군인",
+            "probability": 1.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "자애로운 마음 테두리",
+            "probability": 1.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "수호 기사의 투구 테두리",
+            "probability": 1.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "여왕의 머리 장식",
+            "probability": 1.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "수호 기사의 맹세",
+            "probability": 1.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "환영 트럼프 지갑",
+            "probability": 1,
+            "isEquipItem": true
+        },
+        {
+            "name": "클로버의 잭 사립탐정",
+            "probability": 1.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "하트의 퀸 성직자",
+            "probability": 1.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "스페이드의 킹 교주",
+            "probability": 1.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "다이아몬드의 에이스 판사",
+            "probability": 1.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "조커 도둑",
+            "probability": 1.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "트럼프 지갑",
+            "probability": 1,
+            "isEquipItem": true
+        },
+        {
+            "name": "붉은 여왕의 심장",
+            "probability": 1.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "타락한 왕의 권능",
+            "probability": 1.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "다이아몬드 테두리",
+            "probability": 1.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "레드 하트 테두리",
+            "probability": 1.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "이야기꾼의 거울",
+            "probability": 1.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "타락한 깃털",
+            "probability": 1.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "충성스러운 기사의 수호",
+            "probability": 1.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "화려한 권신의 탐욕",
+            "probability": 1.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "이면 세계의 트럼프 지갑",
+            "probability": 1,
+            "isEquipItem": true
+        },
+        {
+            "name": "신비한 합창",
+            "probability": 1,
+            "isEquipItem": true
+        },
+        {
+            "name": "마법의 염색약",
+            "probability": 4,
+            "isEquipItem": false
+        },
+        {
+            "name": "깜짝 엽서",
+            "probability": 4,
+            "isEquipItem": false
+        },
+        {
+            "name": "징벌의 엽서",
+            "probability": 4,
+            "isEquipItem": false
+        },
+        {
+            "name": "낡은 징벌의 엽서",
+            "probability": 4,
+            "isEquipItem": false
+        },
+        {
+            "name": "주사위 엽서",
+            "probability": 4,
+            "isEquipItem": false
+        },
+        {
+            "name": "왕가의 보물",
+            "probability": 5.5,
+            "isEquipItem": false
+        },
+        {
+            "name": "기사의 잎새",
+            "probability": 5.5,
+            "isEquipItem": false
+        },
+        {
+            "name": "마법의 스페이드 카드",
+            "probability": 5.5,
+            "isEquipItem": false
+        },
+        {
+            "name": "마법의 하트 카드",
+            "probability": 5.5,
+            "isEquipItem": false
+        },
+        {
+            "name": "광대의 확성기",
+            "probability": 5.6,
+            "isEquipItem": false
+        },
+        {
+            "name": "트럼프 엽서",
+            "probability": 5.6,
+            "isEquipItem": false
+        }
+    ],
+    low: [
+        {
+            "name": "황야의 승부사 건달",
+            "probability": 0.7,
+            "isEquipItem": true
+        },
+        {
+            "name": "판의 지배자 마술사",
+            "probability": 0.7,
+            "isEquipItem": true
+        },
+        {
+            "name": "지배자의 코트 테두리",
+            "probability": 0.7,
+            "isEquipItem": true
+        },
+        {
+            "name": "서부의 모험 테두리",
+            "probability": 0.7,
+            "isEquipItem": true
+        },
+        {
+            "name": "승자의 속임수",
+            "probability": 0.7,
+            "isEquipItem": true
+        },
+        {
+            "name": "마지막 베팅",
+            "probability": 0.7,
+            "isEquipItem": true
+        },
+        {
+            "name": "승부사의 지갑",
+            "probability": 0.5,
+            "isEquipItem": true
+        },
+        {
+            "name": "클로버의 퀸 심리학자",
+            "probability": 0.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "다이아몬드의 잭 군인",
+            "probability": 0.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "자애로운 마음 테두리",
+            "probability": 0.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "수호 기사의 투구 테두리",
+            "probability": 0.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "여왕의 머리 장식",
+            "probability": 0.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "수호 기사의 맹세",
+            "probability": 0.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "환영 트럼프 지갑",
+            "probability": 0.1,
+            "isEquipItem": true
+        },
+        {
+            "name": "클로버의 잭 사립탐정",
+            "probability": 0.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "하트의 퀸 성직자",
+            "probability": 0.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "스페이드의 킹 교주",
+            "probability": 0.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "다이아몬드의 에이스 판사",
+            "probability": 0.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "조커 도둑",
+            "probability": 0.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "트럼프 지갑",
+            "probability": 0.1,
+            "isEquipItem": true
+        },
+        {
+            "name": "붉은 여왕의 심장",
+            "probability": 0.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "타락한 왕의 권능",
+            "probability": 0.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "다이아몬드 테두리",
+            "probability": 0.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "레드 하트 테두리",
+            "probability": 0.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "이야기꾼의 거울",
+            "probability": 0.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "타락한 깃털",
+            "probability": 0.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "충성스러운 기사의 수호",
+            "probability": 0.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "화려한 권신의 탐욕",
+            "probability": 0.2,
+            "isEquipItem": true
+        },
+        {
+            "name": "이면 세계의 트럼프 지갑",
+            "probability": 0.1,
+            "isEquipItem": true
+        },
+        {
+            "name": "스포이트",
+            "probability": 4.5,
+            "isEquipItem": false
+        },
+        {
+            "name": "마법의 염색약",
+            "probability": 4.5,
+            "isEquipItem": false
+        },
+        {
+            "name": "신분증",
+            "probability": 4.5,
+            "isEquipItem": false
+        },
+        {
+            "name": "사망 광고판",
+            "probability": 4.5,
+            "isEquipItem": false
+        },
+        {
+            "name": "부고기사",
+            "probability": 4.5,
+            "isEquipItem": false
+        },
+        {
+            "name": "고대의 제작서",
+            "probability": 4.5,
+            "isEquipItem": false
+        },
+        {
+            "name": "마법의 연마제",
+            "probability": 4.5,
+            "isEquipItem": false
+        },
+        {
+            "name": "명장의 망치",
+            "probability": 4.5,
+            "isEquipItem": false
+        },
+        {
+            "name": "큐피드의 화살",
+            "probability": 4.5,
+            "isEquipItem": false
+        },
+        {
+            "name": "고급 엽서",
+            "probability": 4.5,
+            "isEquipItem": false
+        },
+        {
+            "name": "징벌의 엽서",
+            "probability": 4.5,
+            "isEquipItem": false
+        },
+        {
+            "name": "깜짝 엽서",
+            "probability": 4.5,
+            "isEquipItem": false
+        },
+        {
+            "name": "확성기",
+            "probability": 4.5,
+            "isEquipItem": false
+        },
+        {
+            "name": "왕가의 보물",
+            "probability": 5.4,
+            "isEquipItem": false
+        },
+        {
+            "name": "기사의 잎새",
+            "probability": 5.4,
+            "isEquipItem": false
+        },
+        {
+            "name": "마법의 다이아몬드 카드",
+            "probability": 5.4,
+            "isEquipItem": false
+        },
+        {
+            "name": "마법의 하트 카드",
+            "probability": 5.4,
+            "isEquipItem": false
+        },
+        {
+            "name": "트럼프 엽서",
+            "probability": 5.55,
+            "isEquipItem": false
+        },
+        {
+            "name": "광대의 확성기",
+            "probability": 5.55,
+            "isEquipItem": false
+        }
+    ]
+};
